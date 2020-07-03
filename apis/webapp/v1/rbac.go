82cfa991ea38f4c0ad8e5a66436dffe075debe61
Daniel Qian <chanjarster@gmail.com>
2020-07-03 09:39:01 +0800

    add rbac tags

---
+++ apis/webapp/v1/rbac.go
@@ -0,0 +1,4 @@
+// +kubebuilder:rbac:groups=webapp.example.com,resources=guestbooks,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=webapp.example.com,resources=guestbooks/status,verbs=get;update;patch
+
+package v1
